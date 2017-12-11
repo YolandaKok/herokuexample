44153557293c34e3eed71c7ab4e585bdedc0b27b
YolandaKok <yiola8c@gmail.com>
2017-12-12 00:50:38 +0200

    Heroku fix

--- index.js
+++ index.js
@@ -33,6 +33,7 @@ app.get('/books', function (req, res) {
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/node_modules'));
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
@@ -45,5 +46,3 @@ app.get('*', function(request, response) {
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
-
