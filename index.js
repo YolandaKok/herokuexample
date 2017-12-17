0eb56aadcdf75633a061f3a6a4065b2891cc1a65
YolandaKok <yiola8c@gmail.com>
2017-12-17 21:44:46 +0200

    Authentication asynchronous

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var Book = require('./Book');
 var User = require('./User');
+var bcrypt = require('bcrypt');
 
 mongoose.Promise = global.Promise;
 
@@ -49,7 +50,13 @@ app.post('/signup', function (req, res) {
   // Create a new user
   var newUser = User();
   newUser.username = req.body.username;
-  newUser.password = newUser.generateHash(req.body.password);
+  //newUser.password = newUser.generateHash(req.body.password);
+  bcrypt.genSalt(10, function(err, salt) {
+    bcrypt.hash(req.body.password, salt, function(err, hash) {
+        newUser.password = hash;
+    });
+  });
+
   newUser.save(function(err, user) {
     if(err) {
       res.send('Error in sign up');
@@ -59,8 +66,34 @@ app.post('/signup', function (req, res) {
       res.status(200).send(user);
     }
   });
-
 });
+
+// Check if the user inserted the right credentials
+app.post('/signin', function (req, res) {
+  // find if the user exists
+  User.findOne({username : req.body.username}, function (err, user) {
+    if (err) {
+      console.log('Error');
+    }
+    else {
+      // if the user exists
+      // check the hash of this password
+      bcrypt.compare(req.body.password, user.password, function(err, res) {
+        if(res) {
+          // Passwords match
+          console.log('Right Password');
+        }
+        else {
+          // Passwords don't match
+          console.log('Wrong Password');
+        }
+      });
+      // console.log(user.password);
+      // res.status(200);
+    }
+  });
+});
+
 
 
 app.set('port', (process.env.PORT || 5000));
